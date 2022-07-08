db8bb3276e6acf5b0b9409146a06377b42fafd60
DanielJamesF <fredericksd17@gmail.com>
2022-07-08 15:27:56 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,47 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+
+    component: () => import('../views/About.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+
+    component: () => import('../views/Contact.vue')
+  },
+  {
+    path: '/products',
+    name: 'products',
+
+    component: () => import('../views/Products.vue')
+  },
+  {
+    path: '/product',
+    name: 'product',
+
+    component: () => import('../views/Product.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+
+    component: () => import('../views/Admin.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
