2d3e3866fc728310c085021ffd2da476b9bca7b7
Emivw <emiya.vanwyk@gmail.com>
2022-07-11 11:23:03 +0200

    test

---
+++ src/views/Product.vue
@@ -0,0 +1,11 @@
+<template>
+        <card></card>
+</template>
+    
+<script>
+    
+</script>
+    
+<style>
+    
+</style>
---
+++ src/views/Products.vue
@@ -0,0 +1,18 @@
+<template>
+        
+</template>
+    
+<script>
+    import Cards from '@/components/Cards.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Cards
+  }
+}
+</script>
+    
+<style>
+    
+</style>
