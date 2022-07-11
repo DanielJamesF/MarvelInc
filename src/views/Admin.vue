c662ab467911472ef69435f6c7b488b6c75d33f1
taucia <taucia.jackson123@gmail.com>
2022-07-11 16:20:54 +0200

    changes

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,13 @@
 <template>
-  
+ <div>
+    <h2>admin</h2>
+    <div text to="/admin/new">add admin</div>
+    <!-- <div v-for="item of items":key="items.id" class="flex"> -->
+<div>{{admin.title}}</div>
+<div>{{admin.description}}</div>
+<div>show edit delete</div>
+    </div>
+ <!-- </div> -->
 </template>
 
 <script>
@@ -9,5 +17,32 @@ export default {
 </script>
 
 <style>
+h1{
+    text-align: center;
+    color: white;
+    text-decoration: underline;
+    font-style: italic;
+    font-weight: 500;
+    font-size: 5rem;
+}
+table ,th ,td {
+    border: 1px solid grey;
+    /* text-align: center; */
+    padding: 20px;
+}
+table{
+    margin: auto;
+    padding: 50px;
+}
+#img:hover{
+    background-color: grey;
+}
+#footer{
+    padding: 20px;
+    }
 
+
+img{
+    object-fit: cover;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="p-5 bg-light">
+  <div class="container">
+    <h1 class="display-3">MARVEL INC.</h1>
+    <p class="lead">Your one stop shop for all your hero needs</p>
+    <hr class="my-2">
+    <p>Browse our wares</p>
+    <p class="lead">
+      <a class="btn btn-danger btn-lg" href="Jumbo action link" role="button">Items of Interest</a>
+    </p>
+  </div>
+</div>
   </div>
 </template>
 
@@ -12,7 +21,10 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style>
+
+</style>
